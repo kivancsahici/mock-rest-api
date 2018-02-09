ff30b4518dbdde76876b9be87d17f84d494b350b
Sahici <Kivanc.Sahici@bshg.com>
2018-02-09 23:31:46 +0300

    replace customers router with sprints router

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('customers.json')
+const router = jsonServer.router('sprints.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
