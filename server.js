c35ece1b3dbb4d434a62dac621ab4d8f9c0f604c
Sahici <Kivanc.Sahici@bshg.com>
2018-02-10 14:53:27 +0300

    add looney tunes icons

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const router = jsonServer.router('sprints.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+//custom router for query parameters
+server.get('/secure/useravatar', (req, res) => {
+  res.sendFile(__dirname + '/public/icons/' + req.query.ownerId + '.png');
+})
+
+// Use default router
 server.use(router)
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
